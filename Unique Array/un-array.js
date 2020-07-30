bbd0367759b11f83e5129c7ba24ac31c57b1c642
RagsTUD2020 <xiii.chaos666@gmail.com>
2020-07-30 21:41:24 +0300

    Unique array update
    Update unique array function

--- Unique Array/un-array.js
+++ Unique Array/un-array.js
@@ -1,25 +1,41 @@
 let arr = [
     { name: "a", id: 1 },
-    { name: "b", id: 2 },
+    { name: "b", id: 1 },
     { name: "c", id: 3 },
     { name: "d", id: 4 },
     { name: "c", id: 5 },
     { name: "a", id: 6 }, 
     { name: "d", id: 7 }, 
-    { name: "e", id: 8 }]
+    { name: "e", id: 3 }]
   
-  let newArr = arr.reduce((acc, item) => {
-      if (acc.map[item.name])
+
+
+let uniqueNames = arr.reduce((acc, item) => {
+    if (acc.map[item.name])
         return acc;
   
-      acc.map[item.name] = true; 
-      acc.newArr.push(item);
-      return acc; 
+    acc.map[item.name] = true; 
+    acc.uniqueNames.push(item);
+    return acc; 
     }, {
       map: {}, 
-      newArr: [] 
+      uniqueNames: [] 
+    })
+    .uniqueNames;
+
+let newArr = uniqueNames.reduce((acc, item) => {
+    if (acc.map[item.id])
+        return acc;
+      
+    acc.map[item.id] = true; 
+    acc.newArr.push(item);
+    return acc; 
+    }, {
+        map: {}, 
+        newArr: [] 
     })
     .newArr;
+
   
   console.log(newArr);
 
