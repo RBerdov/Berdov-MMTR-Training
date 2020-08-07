877b11e3ed59b48bfada6dd383b33e484c30eb42
RagsTUD2020 <xiii.chaos666@gmail.com>
2020-08-07 23:21:53 +0300

    Login form - welcome page
    Added welcome page styles, added user info

--- LoginForm/js/script.js
+++ LoginForm/js/script.js
@@ -1,3 +1,10 @@
+const newUser = {
+    fullName: '',
+    username: '',
+    email: '',
+    password: ''
+}
+
 submitLogin = (ls) => {
     console.log(ls.parentNode);
 
@@ -21,9 +28,17 @@ submitRegister = (rs) => {
     password = rs.querySelector('#password')
     confirmPass = rs.querySelector('#confirm-password');
     if(fullName.value != '' && username.value != '' && email.value != '' && password.value != '' && confirmPass.value != '') {
-        if(password.value == confirmPass.value) {
+        newUser.fullName = fullName.value;
+        newUser.username = username.value;
+        newUser.email = email.value;
+        newUser.password = password.value;
+        if(newUser.password == confirmPass.value) {
+            console.log(newUser);
             rs.parentNode.style.display = 'none';
             document.querySelector('.welcome').style.display = 'flex'; 
+            document.querySelector('.welcome__title').textContent = 'Welcome, ' + newUser.username + '!';
+            document.querySelector('#welcome-full-name').textContent = 'Your full name: ' + newUser.fullName;
+            document.querySelector('#welcome-email').textContent = 'Your email: ' + newUser.email;
         }
          
     }
---
+++ Rectangles/js/script.js
@@ -0,0 +1,4 @@
+drawRectangles = () => {
+    const rect = document.querySelector('.rectangle').style.border = '1px solid gray';
+}
+
