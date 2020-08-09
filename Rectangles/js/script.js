3bc3e2828bb2d05821fc04996612ac50a0e6ee47
RagsTUD2020 <xiii.chaos666@gmail.com>
2020-08-10 01:06:34 +0300

    Rectangle task (not ready)
    Rectangle task solution but isn't ready yet

--- Rectangles/js/script.js
+++ Rectangles/js/script.js
@@ -1,4 +1,18 @@
-drawRectangles = () => {
-    const rect = document.querySelector('.rectangle').style.border = '1px solid gray';
+drawRectangles = (x, y, rectWidth, rectHeight) => {
+    let w = window.innerWidth;
+    console.log(w/2);
+    let h = window.innerHeight;
+    console.log(h/2);
+    const rect = document.querySelector('.rectangle');
+    console.log(rect);
+    rect.style.top = h/2 + 'px';
+    rect.style.left = w/2 + 'px';
+    rect.style.paddingRight = rectWidth/2 + 'px';
+    rect.style.paddingLeft = rectWidth/2 + 'px';
+    rect.style.paddingTop = rectHeight/2 + 'px';
+    rect.style.paddingBottom = rectHeight/2 + 'px';
+    for(let i = 1; i < 8; i++) {
+
+    } 
 }
 
